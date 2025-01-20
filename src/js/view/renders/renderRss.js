55024dd5ebd6080d45d56b0555f89ee9160bb59a
zzpillau <zzpillau@gmail.com>
2025-01-20 18:06:30 +0200

    refactor: promises for renderRss

--- src/js/view/renders/renderRss.js
+++ src/js/view/renders/renderRss.js
@@ -25,58 +25,64 @@ const renderContainer = (rootSelector, generateConfig) => {
     });
 };
 
-const renderContent = (config) => {
-  renderContainer(config.rootSelector, config.containerFunc)
-    .then(() => {
-      config.list.forEach((element) => {
-        const root = document.querySelector(config.secondarySelector);
-        const paramsValues = config.params.map((param) => element[param]);
-
-        const renderElement = (i18n) => {
-          const newElConfig = config.needsI18n
-            ? config.elementFunc(...paramsValues, i18n, config.eHandler)
-            : config.elementFunc(...paramsValues);
-          new HTMLBuilder(newElConfig).render(root);
-        };
-
-        if (config.needsI18n) {
-          getInstanceI18n().then(renderElement);
-        } else {
-          renderElement();
-        }
-      });
-    })
-    .catch((error) => {
-      console.error('renderContent Error', error);
-    });
+const renderElement = (config, root, params, i18n) => {
+  const newElConfig = config.needsI18n
+    ? config.elementFunc(...params, i18n, config.eHandler)
+    : config.elementFunc(...params);
+  new HTMLBuilder(newElConfig).render(root);
 };
+
+const handleElementRendering = (config, element, root) => {
+  const paramsValues = config.params.map((param) => element[param]);
+  if (config.needsI18n) {
+    return getInstanceI18n()
+      .then((i18n) => renderElement(config, root, paramsValues, i18n));
+  }
+  renderElement(config, root, paramsValues);
+  return Promise.resolve();
+};
+
+const renderContent = (config) => renderContainer(config.rootSelector, config.containerFunc)
+  .then(() => {
+    const root = document.querySelector(config.secondarySelector);
+    const renderPromises = config.list
+      .map((element) => handleElementRendering(config, element, root));
+
+    return Promise.all(renderPromises);
+  })
+  .catch((error) => {
+    console.error('renderContent Error', error);
+  });
+
+const createFeedConfig = (feedList) => ({
+  list: feedList,
+  rootSelector: '.feeds',
+  secondarySelector: '.feed-list',
+  containerFunc: generateFeedContainerConfig,
+  elementFunc: generateFeedConfig,
+  params: ['title', 'description'],
+  needsI18n: false,
+});
+
+const createPostConfig = (postsList, eHandler) => ({
+  list: postsList,
+  rootSelector: '.posts',
+  secondarySelector: '.posts-list',
+  containerFunc: generatePostsContainerConfig,
+  elementFunc: generatePostConfig,
+  params: ['id', 'title', 'url', 'isRead'],
+  needsI18n: true,
+  eHandler: { event: 'click', handler: eHandler },
+});
 
 const renderRss = (state, eHandler) => {
   const { feedList, postsList } = state.rssProcess;
 
-  const feedConfig = {
-    list: feedList,
-    rootSelector: '.feeds',
-    secondarySelector: '.feed-list',
-    containerFunc: generateFeedContainerConfig,
-    elementFunc: generateFeedConfig,
-    params: ['title', 'description'],
-    needsI18n: false,
-  };
-
-  const postConfig = {
-    list: postsList,
-    rootSelector: '.posts',
-    secondarySelector: '.posts-list',
-    containerFunc: generatePostsContainerConfig,
-    elementFunc: generatePostConfig,
-    params: ['id', 'title', 'url', 'isRead'],
-    needsI18n: true,
-    eHandler: { event: 'click', handler: eHandler },
-  };
-
-  renderContent(feedConfig);
-  renderContent(postConfig);
+  return renderContent(createFeedConfig(feedList))
+    .then(() => renderContent(createPostConfig(postsList, eHandler)))
+    .catch((err) => {
+      console.error('renderRss error:', err);
+    });
 };
 
 export default renderRss;
